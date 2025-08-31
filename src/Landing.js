42bb96cf23fe9237126a86d7e975d62db925d842
Saksham Soni <sonisaksham20208@gmail.com>
2025-08-31 12:53:35 +0530

    Update Landing.js

--- src/Landing.js
+++ src/Landing.js
@@ -9,10 +9,10 @@ const Landing = () => {
     <div data-scroll 
         data-scroll-section 
         data-scroll-speed="-.3" 
-    className="relative w-100 h-screen overflow-hidden z-1">
+    className="md:relative md:w-100 md:h-screen md:overflow-hidden md:z-1   relative w-100 h-100 overflow-hidden z-1">
 
       <motion.video variants={fadeIn("up", 0.2)} initial="hidden" whileInView={"show"} viewport={{ once: false, amount: 0.2 }}
-        className="w-[80%] mx-auto  my-16"
+        className="w-[90%] mt-20 mx-auto md:w-[80%] md:mx-auto  md:my-20"
         src={vid}
         autoPlay
         muted
@@ -24,8 +24,3 @@ const Landing = () => {
   );
 };
 export default Landing;
-
-
-
-
-
